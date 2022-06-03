7967e859b78cdabc0f3a3c23d89cf57479ca026d
ota <ryusei20002@gmail.com>
2022-06-03 20:30:30 +0900

    いったん、CRUD機能完成

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -11,26 +11,31 @@ import { DeleteOnetodoArgs } from 'src/@generated/prisma-nestjs-graphql/todo/del
 export class TodoResolver {
   constructor(private readonly todoService: TodoService) {}
 
+  //1件取得
   @Query(() => todo)
   todo(@Args() args: FindFirsttodoArgs) {
     return this.todoService.findFirst(args);
   }
 
+  //全件取得
   @Query(() => [todo])
   async findAll(@Args() args: FindManytodoArgs) {
     return await this.todoService.findAll(args);
   }
 
+  //登録
   @Mutation(() => todo)
   async createTodo(@Args() args: CreateOnetodoArgs) {
     return this.todoService.createTodo(args);
   }
 
+  //更新
   @Mutation(() => todo)
   async updateTodo(@Args() args: UpdateOnetodoArgs) {
     return this.todoService.updateTodo(args);
   }
 
+  //削除
   @Mutation(() => todo)
   async deleteTodo(@Args() args: DeleteOnetodoArgs){
     return this.todoService.deleteTodo(args);
