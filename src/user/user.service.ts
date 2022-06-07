be7a8d53c92d3b4a582d8a29b7f9110fc01b5351
ota <ryusei20002@gmail.com>
2022-06-08 01:25:37 +0900

    わからなくなった

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,11 +12,6 @@ import { UpdateOneUserArgs } from 'src/@generated/prisma-nestjs-graphql/user/upd
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  //登録
-  async createUser(args: CreateOneUserArgs) {
-    return this.prisma.user.create(args);
-  }
-
   //削除
   async deleteUser(args: DeleteOneUserArgs) {
     return this.prisma.user.delete(args);
@@ -27,15 +22,19 @@ export class UsersService {
     return await this.prisma.user.findMany();
   }
 
-  async findUnique(args: FindUniqueUserArgs): Promise<User | null> {
-    return this.prisma.user.findUnique(args);
-  }
-
   async findFirst(args: FindFirstUserArgs): Promise<User | null> {
     return this.prisma.user.findFirst(args);
 }
- 
+
+async findUnique(args: FindUniqueUserArgs): Promise<User | null> {
+    return this.prisma.user.findUnique(args);
+}
+
+async createUser(args: CreateOneUserArgs): Promise<User> {
+    return this.prisma.user.create(args);
+}
+
   async update(args: UpdateOneUserArgs): Promise<User> {
-    return this.prisma.user.update(args)
+      return this.prisma.user.update(args)
   }
 }
